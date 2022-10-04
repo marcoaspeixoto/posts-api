88397c27e859d5f6ceac09fad73496e11390b8f9
Marco Peixoto <marcoaspeixto@gmail.com>
2022-10-04 20:52:07 -0300

    feat(posts): controller create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,24 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.all
 
-    render json:@posts
+    render json: @posts
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :content, :author)
   end
 end
