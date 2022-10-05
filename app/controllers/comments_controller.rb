5a27c0ab7f9ed4e5ccedae2d1038f5b9fc88ace3
Marco Peixoto <marcoaspeixto@gmail.com>
2022-10-05 07:08:44 -0300

    refactor(comment): delete requerie comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -46,6 +46,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:post_id, :author, :content)
+      params.permit(:post_id, :author, :content)
     end
 end
